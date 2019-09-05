66decb1cbac4394b762e236cf5db9cc1617bffbc
toto <toto>
2019-09-05 10:06:29 +0000

    first VM

--- project1/main.tf
+++ project1/main.tf
@@ -4,6 +4,56 @@
 ##
 #####################################################################
 
+## REFERENCE {"vsphere_network":{"type": "vsphere_reference_network"}}
+
 terraform {
   required_version = "> 0.8.0"
+}
+
+provider "vsphere" {
+  user           = "${var.user}"
+  password       = "${var.password}"
+  vsphere_server = "${var.vsphere_server}"
+
+  allow_unverified_ssl = "${var.allow_unverified_ssl}"
+  version = "~> 1.2"
+}
+
+
+data "vsphere_virtual_machine" "maVM_template" {
+  name          = "${var.maVM_template_name}"
+  datacenter_id = "${data.vsphere_datacenter.maVM_datacenter.id}"
+}
+
+data "vsphere_datacenter" "maVM_datacenter" {
+  name = "${var.maVM_datacenter_name}"
+}
+
+data "vsphere_datastore" "maVM_datastore" {
+  name          = "${var.maVM_datastore_name}"
+  datacenter_id = "${data.vsphere_datacenter.maVM_datacenter.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "${var.network_network_name}"
+  datacenter_id = "${data.vsphere_datacenter.maVM_datacenter.id}"
+}
+
+resource "vsphere_virtual_machine" "maVM" {
+  name          = "${var.maVM_name}"
+  datastore_id  = "${data.vsphere_datastore.maVM_datastore.id}"
+  num_cpus      = "${var.maVM_number_of_vcpu}"
+  memory        = "${var.maVM_memory}"
+  guest_id = "${data.vsphere_virtual_machine.maVM_template.guest_id}"
+  resource_pool_id = "${var.maVM_resource_pool}"
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.maVM_template.id}"
+  }
+  disk {
+    name = "${var.maVM_disk_name}"
+    size = "${var.maVM_disk_size}"
+  }
 }
--- project1/variables.tf
+++ project1/variables.tf
@@ -4,3 +4,73 @@
 ##
 #####################################################################
 
+variable "user" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "password" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "vsphere_server" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "allow_unverified_ssl" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "maVM_name" {
+  type = "string"
+  description = "Virtual machine name for maVM"
+}
+
+variable "maVM_number_of_vcpu" {
+  type = "string"
+  description = "Number of virtual cpu's."
+}
+
+variable "maVM_memory" {
+  type = "string"
+  description = "Memory allocation."
+}
+
+variable "maVM_disk_name" {
+  type = "string"
+  description = "The name of the disk. Forces a new disk if changed. This should only be a longer path if attaching an external disk."
+}
+
+variable "maVM_disk_size" {
+  type = "string"
+  description = "The size of the disk, in GiB."
+}
+
+variable "maVM_template_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "maVM_datacenter_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "maVM_datastore_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "maVM_resource_pool" {
+  type = "string"
+  description = "Resource pool."
+}
+
+variable "network_network_name" {
+  type = "string"
+  description = "Generated"
+}
+
